7bcbf90abeaeb6871deb433a54b4eadd59eb90f0
Efye Ching <efyeching848@gmail.com>
2025-06-20 22:12:02 +0800

    Change Pi base URL

--- app/src/main/java/com/example/wateryourplant/data/network/RaspPiLocalService.kt
+++ app/src/main/java/com/example/wateryourplant/data/network/RaspPiLocalService.kt
@@ -3,7 +3,7 @@ package com.example.wateryourplant.data.network
 import com.example.wateryourplant.data.api.RaspPiApiService
 
 object RaspPiLocalService {
-    private const val RASP_PI_BASE_URL = "http://172.20.10.3:5000/"
+    private const val RASP_PI_BASE_URL = "http://115.134.176.25:5000/"
 
     val api: RaspPiApiService by lazy {
         RetrofitFactory.create(RASP_PI_BASE_URL).create(RaspPiApiService::class.java)
